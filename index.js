7da607317dbe922d458adb33c8484f95acd509ee
jibstack64 <jibbystar@proton.me>
2022-12-04 17:02:38 +0000

    Mobile works!

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function applyCss() {
     var st = "";
     for (var i = 0; i < styling.length; i++) {
         if ((i % 54) == 0 && i > 0 || (styling[i+1] == "}" && styling[i] != ";") || styling[i-1] == "{" || styling[i-1] == ";") {
-            st += "\n"
+            st += "\n";
         }
-        st += styling[i]
+        st += styling[i];
     }
-    var code = document.getElementById("code")
+    var code = document.getElementById("code");
     code.value = st;
     // add to style object
     var ps = document.head.getElementsByTagName("style");
@@ -23,5 +23,13 @@ function applyCss() {
         ps.innerHTML = styling;
         document.head.append(ps);
         return;
+    }
+}
+
+// loads the appropriate styling depending on whether the user is on mobile or pc
+function loadAppropriateStylings() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var elem = document.head.getElementsByTagName("link");
+        elem[0]["href"] = "mobile.css";
     }
 }
