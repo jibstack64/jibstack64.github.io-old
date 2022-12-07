90128b8da4183ba925abf4a07f371fd9f46f5c3f
jibstack64 <jibbystar@proton.me>
2022-12-07 18:36:18 +0000

    Sorted suggestions system + patched some bugs.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ip and port for suggestions server
-const IP = "localhost";
+const IP = "127.0.0.1";
 const PORT = 5152;
 const URL = "http://" + IP + ":" + PORT
 
@@ -37,7 +37,12 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
+    var sl = slides[slideIndex-1];
+    if (sl == undefined) {
+        return;
+    } else {
+        slides[slideIndex-1].style.display = "block";
+    }
 } 
 
 // loads the appropriate styling depending on whether the user is on mobile or pc
@@ -58,34 +63,58 @@ function loadAppropriateStylings() {
 
 function suggestionsUpVisual() {
     var status = document.getElementById("status");
+    var input = document.getElementById("suggestion-input");
+    var submit = document.getElementById("submit-button")
+    var infield = document.getElementById("infields");
+    function applyStyle(success) {
+        if (success) {
+            status.className = "online";
+            status.innerHTML = "ONLINE";
+            submit["style"] = "";
+            infield["style"] = "";
+            submit.className = "hlinked";
+            submit.removeAttribute("disabled");
+            input["style"] = "";
+            input.removeAttribute("disabled");
+        } else {
+            status.className = "offline";
+            status.innerHTML = "OFFLINE";
+            submit["style"] = "cursor: not-allowed;";
+            submit.className = "hlinked-nohover";
+            submit["disabled"] = "";
+            infield["style"] = "cursor: not-allowed;"
+            input["style"] = "pointer-events: none;";
+            input["disabled"] = "";
+        }
+    }
     let xhr = new XMLHttpRequest();
     xhr.open("GET", URL + "/up");
     xhr.send();
     xhr.onload = function() {
-        if (xhr.status == 200) {
-            status.className = "online";
-            status.innerHTML = "ONLINE";
-            updateSuggestions();
+        if (xhr.status != 200) {
+            applyStyle(false);
         } else {
-            status.className = "offline";
-            status.innerHTML = "OFFLINE";
+            applyStyle(true);
         }
+        updateSuggestions();
     }
     xhr.onerror = function() {
-        status.className = "offline";
-        status.innerHTML = "OFFLINE";
+        applyStyle(false);
+    }
+}
+
+function voteFor(id) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", URL + "/vote")
+    xhr.send(id);
+    xhr.onload = function() {
+        alert(xhr.responseText);
+        updateSuggestions();
     }
 }
 
 function updateSuggestions() {
     var sugTable = document.getElementById("suggestions-table");
-    for (x of sugTable.getElementsByTagName("tr")) {
-        if (x.id == "dont-touch") {
-            continue;
-        } else {
-            sugTable.removeChild(x);
-        }
-    }
     let xhr = new XMLHttpRequest();
     xhr.open("GET", URL + "/suggestions");
     xhr.send();
@@ -97,11 +126,24 @@ function updateSuggestions() {
             for (x of JSON.parse(xhr.response)) {
                 var newElem = document.createElement("tr");
                 newElem.id = x["id"];
+                newElem.style = "display: flex; align-items: stretch;"
                 var newElemSugg = document.createElement("th");
                 newElemSugg.innerHTML = x["suggestion"];
+                newElemSugg.className = "suggestion-table-pusher";
+                newElemSugg.style = "word-wrap: break-word;"
                 var newElemVotes = document.createElement("th");
                 newElemVotes.innerHTML = x["votes"];
-                newElem.append(newElemSugg, newElemVotes);
+                newElemVotes.className = "vote-num";
+                var newElemButton = document.createElement("button");
+                newElemButton.innerHTML = "Vote";
+                newElemButton.className = "vote-button";
+                newElemButton.style = "float: center; height: auto;"
+                newElemButton.setAttribute("onclick", "voteFor(\"" + String(x["id"]) + "\")");
+                var childToRemove = document.getElementById(x["id"]);
+                if (childToRemove != undefined && childToRemove != null) {
+                    sugTable.removeChild(childToRemove);
+                }
+                newElem.append(newElemSugg, newElemVotes, newElemButton);
                 sugTable.appendChild(newElem);
             }
         }
